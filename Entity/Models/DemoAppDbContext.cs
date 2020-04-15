39681042c4ade51b338d7c547eda5b23f22024f6
ThapLV <thaplv176879@sis.hust.edu.vn>
2020-04-15 14:08:53 +0700

    init

---
+++ Entity/Models/DemoAppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity.Models
+{
+   public class DemoAppDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=TENHANDSOME\SQLEXPRESS;Initial Catalog=DemoApp;Integrated Security=True");
+        }
+    }
+}
---
+++ Entity/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Entity.Models
+{
+    public class Student
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+
+        public int name { get; set; }
+    }
+}
